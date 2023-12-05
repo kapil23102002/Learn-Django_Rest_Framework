6d8de8aa45aa9d45107fe2bef38391a05f16e6db
KAPIL THAKUR <KAPPU445@GMAIL.com>
2023-12-05 14:55:06 +0530

    get all JSON data by using serialzer

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,8 @@ from api import views
 
 urlpatterns = [
     path('', views.home),
-    path('student_details/<int:pk>', views.student_details, name = 'student_details')
+    path('student_details/<int:pk>', views.student_details, name = 'student_details'), 
+    path('student_details/', views.student_info, name = 'student_details')
+
     
 ]
--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Student
 from .serializers import StudentSerializier
 from rest_framework.renderers import JSONRenderer
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def home(request):
@@ -12,5 +12,15 @@ def home(request):
 def student_details(request, pk):
     stu = Student.objects.get(id = pk) # get data from model object by dynamically
     serializer = StudentSerializier(stu) #convert into python data
-    json_data = JSONRenderer().render(serializer.data) # convert into JSON data
-    return HttpResponse(json_data, content_type='application/json')
+    
+    # json_data = JSONRenderer().render(serializer.data) # convert into JSON data
+    # return HttpResponse(json_data, content_type='application/json')
+# replace lower line from upper 2 lines----------
+    return JsonResponse(serializer.data)
+
+# Quesry Set - All Student Data
+def student_info(request):
+    stu = Student.objects.all()# get all data from model object
+    serializer = StudentSerializier(stu, many = True) #convert into python data
+    return JsonResponse(serializer.data)
+    
