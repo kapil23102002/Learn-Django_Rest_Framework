9e321ca41380f48ef67ffcfa2c2797e73f35fb04
KAPIL THAKUR <KAPPU445@GMAIL.com>
2023-12-05 14:37:47 +0530

    get JSON data by using serialzer

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home),
+    path('student_details/<int:pk>', views.student_details, name = 'student_details')
+    
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Student
+from .serializers import StudentSerializier
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     return render(request,  'home.html')
+
+# Model Object - Single Student data
+def student_details(request, pk):
+    stu = Student.objects.get(id = pk) # get data from model object by dynamically
+    serializer = StudentSerializier(stu) #convert into python data
+    json_data = JSONRenderer().render(serializer.data) # convert into JSON data
+    return HttpResponse(json_data, content_type='application/json')
